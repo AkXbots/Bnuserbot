ae51a6ecacd47b3588f9a85d82a47cbd39ba429d
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-03-02 11:22:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID","5940604852"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/2ac1c953d54544cc79dda.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "1AZWarzQBuwZ-NO-VtCpkfP2LXM2HFTQhtxCFNUiEeIuTOKxOsQQsRNQH2Flwj8gqEdrzrqFGZ7j1Y_qcB2S3N-6Q-grFx_kevFhTz3WNSTU_Jt36hpSCfaRHmcW5xQKSvu0IWdNPfl2RRaOfMBPXNivma0n__BajaQDlfrpGkbZLHJMZitlnt817847D5w7nAE6odh_qyYWk52GNYCxM2YP50YH6br4OMqoMw-aY3mxbNoWMmk_PQe-WZ4fyKDZgTeoXo8UzMVxuzUzAYxSZNkZEuNvj1wFHs3mVU8_grHae_wA7hDCPabe-OgmjlO1xjaUR__Wc-QW-kNfSioVb62wj6honb0g= ")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
